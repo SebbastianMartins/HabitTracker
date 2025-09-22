9c06cb36abe7f6fd564bb9acf1f83ea26a681e7e
Sebas <sebastianmartins177@gmail.com>
2025-09-22 11:17:34 +0200

    Setup inicial: React con Vite Tailwind y TypeS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+    function App() {
+         return (
+           <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+             <h1 className="text-4xl font-bold text-blue-600">
+               ¡Hola! Mi Organizador de Hábitos está en construcción 🚀
+             </h1>
+           </div>
+         );
+       }
+       export default App;
